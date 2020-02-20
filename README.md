34a3423f98c6874ca9efd89d5fba3a25a6f567a4
Michael Greco <michael.greco07@gmail.com>
2020-02-19 19:07:17 -0500

    API setup and Post request

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Night Class Mongo Practice
 
-[Live Demo]()
+[Live Demo](https://michaelgreco7.github.io/Mongo-Practice-1/)
--- server.js
+++ server.js
@@ -17,6 +17,31 @@ mongoose.connect(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+app.post("/user", async (request, response) => {
+  try {
+    console.log("POST USER");
+    var userInstance = new UserModel(request.body);
+    console.log(userInstance);
+    const created = await UserModel.create(userInstance);
+    response.send(created);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
 const start = () => {
   return app.listen(PORT, () => console.log(`server is running on ${PORT}`));
 };
